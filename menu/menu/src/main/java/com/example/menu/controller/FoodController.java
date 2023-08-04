f6c40d270d736160c69c8d897fb8ead67874e026
Murilo Damario <murilo.gcdamario@gmail.com>
2023-08-04 17:19:57 -0300

    Cria método Post e records FoodResponseDTO e FoodRequestDTO

--- menu/menu/src/main/java/com/example/menu/controller/FoodController.java
+++ menu/menu/src/main/java/com/example/menu/controller/FoodController.java
@@ -2,10 +2,10 @@ package com.example.menu.controller;
 
 import com.example.menu.food.Food;
 import com.example.menu.food.FoodRepository;
+import com.example.menu.food.FoodRequestDTO;
+import com.example.menu.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +14,15 @@ import java.util.List;
 public class FoodController {
     @Autowired
     private FoodRepository foodRepository;
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data) {
+        Food foodData = new Food(data);
+        foodRepository.save(foodData);
+        return;
+    }
     @GetMapping
-    public List<Food> getAllFoods() {
-        return foodRepository.findAll();
+    public List<FoodResponseDTO> getAllFoods() {
+        return foodRepository.findAll().stream().map(FoodResponseDTO::new).toList();
     }
 }
---
+++ menu/menu/src/main/java/com/example/menu/food/FoodRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.menu.food;
+
+public record FoodRequestDTO(String title, String image, Integer price) {
+}
