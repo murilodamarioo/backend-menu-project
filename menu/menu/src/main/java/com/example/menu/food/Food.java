8ec56e506c296dcb607a210c7d59f4740a818fa5
Murilo Damario <murilo.gcdamario@gmail.com>
2023-08-07 00:01:22 -0300

    Criação dos métodos get da classe Food

--- menu/menu/src/main/java/com/example/menu/food/Food.java
+++ menu/menu/src/main/java/com/example/menu/food/Food.java
@@ -19,9 +19,25 @@ public class Food {
     private String image;
     private Integer price;
 
-    public  Food(FoodRequestDTO data) {
+    public Food(FoodRequestDTO data) {
         this.image = data.image();
         this.price = data.price();
         this.title = data.title();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Integer getPrice() {
+        return price;
     }
 }
--- menu/menu/src/main/java/com/example/menu/food/FoodRepository.java
+++ menu/menu/src/main/java/com/example/menu/food/FoodRepository.java
@@ -3,5 +3,4 @@ package com.example.menu.food;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
-
 }
