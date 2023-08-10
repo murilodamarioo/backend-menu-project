e9b5fbff4227eb94b3c1123aff74bbd4971a59d3
Murilo Damario <murilo.gcdamario@gmail.com>
2023-08-10 09:30:44 -0300

    Cria métodos de atualizar e deletar Food

--- menu/menu/src/main/java/com/example/menu/controller/FoodController.java
+++ menu/menu/src/main/java/com/example/menu/controller/FoodController.java
@@ -5,9 +5,11 @@ import com.example.menu.food.FoodRepository;
 import com.example.menu.food.FoodRequestDTO;
 import com.example.menu.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("food")
@@ -26,5 +28,38 @@ public class FoodController {
     @GetMapping
     public List<FoodResponseDTO> getAllFoods() {
         return foodRepository.findAll().stream().map(FoodResponseDTO::new).toList();
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateFoodById(@PathVariable Long id, @RequestBody FoodRequestDTO updatedFood) {
+        Optional<Food> optionalFood = foodRepository.findById(id);
+
+        if (optionalFood.isPresent()) {
+            Food existingFood = optionalFood.get();
+
+            existingFood.setTitle(updatedFood.title());
+            existingFood.setImage(updatedFood.image());
+            existingFood.setPrice(updatedFood.price());
+
+            foodRepository.save(existingFood);
+
+            return ResponseEntity.ok("Prato atualizado");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteFood(@PathVariable Long id) {
+        Optional<Food> foodOptional = foodRepository.findById(id);
+
+        if(foodOptional.isPresent()) {
+            foodRepository.deleteById(id);
+            return ResponseEntity.ok("Prato deletado");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
